3aa4da17b1f0e4d7525ba0b7af1fd7645ab6701e
Al Rix <al.rix@pm.me>
2020-06-08 11:10:06 +0100

    updating setup

--- setup.sh
+++ setup.sh
@@ -1,4 +1,15 @@
-# Download bash git prompt
-git clone https://github.com/magicmonty/bash-git-prompt.git ~/repos/github/magicmonty/bash-git-prompt
+# Grab some details
+if [ -f /etc/lsb_release ] ; then
+    DISTRIBUTION=`cat /etc/lsb_release | grep DISTRIB_ID | cut -d"=" -f2`
+fi
 
+# Download bash git prompt
+if [ ! -d ~/repos/github/magicmonty/bash-git-prompt ] ; then
+   git clone https://github.com/magicmonty/bash-git-prompt.git ~/repos/github/magicmonty/bash-git-prompt
+fi
+
+# Install some utilities
+if [ DISTRIBUTION = "Ubuntu" ] ; then
+    apt-get install -y vim awscli ranger
+fi
 
